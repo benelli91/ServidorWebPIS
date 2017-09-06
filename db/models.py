533495cc1c4706243cd6e4af65038e6e211a4170
Santiago Behak <sbehak@gmail.com>
2017-09-06 15:58:46 -0300

    algunas correcciones

--- db/models.py
+++ db/models.py
@@ -88,6 +88,10 @@ class City(models.Model):
         db_table = 'city'
         unique_together = (('id', 'country'),)
 
+    def __str__(self):
+        return str(self.id) + '-' +str(self.country) + '-' + self.name + '\n'
+
+
 
 class Country(models.Model):
     id = models.CharField(primary_key=True, max_length=3)
@@ -96,6 +100,9 @@ class Country(models.Model):
     class Meta:
         managed = False
         db_table = 'country'
+
+    def __str__(self):
+        return str(self.id) + '-' + self.name + '\n'
 
 
 class DjangoAdminLog(models.Model):
@@ -170,3 +177,6 @@ class Traveltype(models.Model):
     class Meta:
         managed = False
         db_table = 'traveltype'
+
+    def __str__(self):
+        return str(self.traveltype) + '-' + self.travelname + '\n'
--- db/views.py
+++ db/views.py
@@ -44,17 +44,6 @@ class TravelDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Travel.objects.all()
     serializer_class = TravelSerializer
 
-def heuristic(city1, city2):
-    return pow((math.pi/180)*(city1.latitude - city2.latitude), 2) + pow(math.cos((math.pi/180)*(city1.latitude-city2.latitude)/2)*((math.pi/180)*(city1.longitude - city2.longitude)),2)
-
-def reconstruct_path(cameFrom, current, countries, origin_country, origin_city):
-    total_path = [cameFrom[countries.index(current[0])][current[1]][current[2]][3]]
-    while not ((current[0] == origin_country) and (current[1] == origin_city)):
-        current = cameFrom[countries.index(current[0])][current[1]][current[2]]
-        if not(current[0] == origin_country and current[1] == origin_city):
-            total_path.append(cameFrom[countries.index(current[0])][current[1]][current[2]][3])
-    return total_path
-
 def Eppstein(origin_country, origin_city, destination_country, destination_city, start_date, finish_date, k):
     start_city = City.objects.filter(country = origin_country, id = origin_city)[0]
     final_city = City.objects.filter(country = destination_country, id = destination_city)[0]
@@ -66,28 +55,27 @@ def Eppstein(origin_country, origin_city, destination_country, destination_city,
 
     P = []
     B = []
-    countU = [[None]*len(cities)]*len(countries)
-    for ci in cities:
-        countU[countries.index(ci.country)][ci.id] = 0
-
+    countU = [[0]*(len(cities)+1) for i in range(len(countries))]
     B.append([[start_city], 0])
-    while len(B) > 0 and countFinal < k:
+    while len(B) > 0 and countU[countries.index(destination_country)][destination_city] < k:
         Pu = B[0]
         for b in B:
             if b[1] < Pu[1]:
                 Pu = b
         B.remove(b)
-        countU[countries.index(Pu[0][-1].country)][Pu[0][-1].id] += 1
+        countU[countries.index(Pu[0][-1].country)][Pu[0][-1].id] = countU[countries.index(Pu[0][-1].country)][Pu[0][-1].id] + 1
         if Pu[0][-1].country == destination_country and Pu[0][-1].id == destination_city:
+            print("llegue aqui")
             P.append(Pu)
         if countU[countries.index(Pu[0][-1].country)][Pu[0][-1].id] <= k:
             neighbors = []
             if Pu[0][-1].country == origin_country and Pu[0][-1].id == origin_city:
                 neighbors = Travel.objects.raw('''SELECT * FROM travel WHERE origin_country = %s AND origin_city = %s AND departure > %s AND departure + duration < %s''', [Pu[0][-1].country, Pu[0][-1].id, start_date, finish_date])
             else:
-                arrival_interval = datetime.combine(P[0][-2].departure, P[0][-2].duration) - start_date
+                last_travel = Pu[0][-2]
+                arrival_interval = datetime.combine(last_travel.departure, last_travel.duration) - start_date
                 arrival = int(arrival_interval.total_seconds() // 3600)
-                neighbors = Travel.objects.raw('''SELECT * FROM travel WHERE origin_country = %s AND origin_city = %s AND departure > %s AND departure + duration < %s''', [Pu[0][-1].country, Pu[0][-1].id, start_date + timedelta(hours = arrival]), finish_date])
+                neighbors = Travel.objects.raw('''SELECT * FROM travel WHERE origin_country = %s AND origin_city = %s AND departure > %s AND departure + duration < %s''', [Pu[0][-1].country, Pu[0][-1].id, start_date + timedelta(hours = arrival), finish_date])
 
             for n in neighbors:
                 flag = False
@@ -97,19 +85,27 @@ def Eppstein(origin_country, origin_city, destination_country, destination_city,
                         flag = True
                     counter += 2
                 if not flag:
-                    Pv = Pu
+                    Pv = [[]]
                     middle_city = City.objects.filter(country = n.destination_country, id = n.destination_city)[0]
+                    for i in range(0, len(Pu[0])):
+                        Pv[0].append(Pu[0][i])
                     Pv[0].append(n)
                     Pv[0].append(middle_city)
-                    Pv[1] += n.price
-                    print(Pu, Pv)
+                    Pv.append(Pu[1] + n.price)
+                    print("Pu: \n")
+                    print(Pu)
+                    print("Pv: \n")
+                    print(Pv)
                     B.append(Pv)
-    result = [[]]
+
+    print(len(B))
+    result = []
     counter = 0
     for p_aux in P:
         for i in range(1, len(p_aux[0])//2):
             result[counter].append(p_aux[0][i*2 - 1])
-    return result
+        counter+=1
+    return P
 
 
 def index(request):
@@ -119,10 +115,13 @@ def index(request):
     vOrigin_city = 6
     vDestination_country = 'ARG'
     vDestination_city = 29
+    k = 5
     print(str(datetime.now()) + '  --inicio' )
-    list_travels = AAster(vOrigin_country, vOrigin_city, vDestination_country, vDestination_city, start_date, finish_date, 3)
+    list_travels = Eppstein(vOrigin_country, vOrigin_city, vDestination_country, vDestination_city, start_date, finish_date, k)
     print(str(datetime.now())+ '  --fin')
-    print len(list_travels)
+    print(len(list_travels))
+
+
     context = {
     'latest_question_list': list_travels}
     return render(request, 'db/index.html', context)
