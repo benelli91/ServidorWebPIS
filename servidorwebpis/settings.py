79fee6ef547800aea871fe90e7410dcbdecd2e5e
Rodrigo_Linares <r93linares@gmail.com>
2017-09-11 18:40:33 -0300

    cambios pedidos por el cliente

--- servidorwebpis/settings.py
+++ servidorwebpis/settings.py
@@ -77,9 +77,9 @@ WSGI_APPLICATION = 'servidorwebpis.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pis_db',
-        'USER': 'jalvez',
-        'PASSWORD': 'pis2017...',
+        'NAME': 'pis',
+        'USER': 'rodrigo',
+        'PASSWORD': '3915',
         'HOST': 'localhost',
         'PORT': '',
     }
