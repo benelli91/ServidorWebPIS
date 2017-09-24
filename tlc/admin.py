b1de0197b2c0472803b7571c7ac21815ee7e6152
joaquin scocozza <joaquinscocozza94@hotmail.com>
2017-09-24 11:59:20 -0300

    Added: admin.py configuration for django admin

--- tlc/admin.py
+++ tlc/admin.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
-
+from .models import City, Country, Travel, Traveltype
 # Register your models here.
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    list_display = ('id', 'name', 'country')
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    search_fields = ('name', )
+    list_display = ('id', 'name')
+
+@admin.register(Travel)
+class TravelAdmin(admin.ModelAdmin):
+    search_fields = ('idtravel', )
+    list_display = ('idtravel', 'departure', 'origin_city',
+        'destination_city', 'price', 'duration', 'traveltype',
+        'description')
+
+@admin.register(Traveltype)
+class TravelTypeAdmin(admin.ModelAdmin):
+    search_fields = ( 'travelname', )
+    list_display = ('traveltype', 'travelname')
