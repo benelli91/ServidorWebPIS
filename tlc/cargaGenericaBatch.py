4f24734d1b6c6d9303fed2bfb88bdd2374317663
Santiago Behak <sbehak@gmail.com>
2017-09-27 19:59:13 -0300

    avance en las paginas de tipo simple y otras mejoras

--- tlc/cargaGenericaBatch.py
+++ tlc/cargaGenericaBatch.py
@@ -9,6 +9,7 @@ import sys
 import os
 import json
 from django.db import transaction
+import unidecode
 
 DEFAULT_SPAN = 30
 
@@ -40,32 +41,45 @@ def loadWebpage(conf_file):
         print dates[-1]
         dates.append(dates[-1] + timedelta(days=1))
 
-
     travels = []
     url = conf_file["webpage"]["uri_start"]
+    page_type = conf_file["webpage"]["page_type"]
 
-    if(conf_file["webpage"]["simple_page"] == False):
+    if(page_type == 1): #URL type pages
         for origin_city in cities:
             for destination_city in cities:
                 if(origin_city.id != destination_city.id):
                     if(conf_file["webpage"]["frecuency_format"] == ""):
                         for departure in dates:
-                            if(conf_file["webpage"]["javascript"] == False):
-                                output_HTML = createURL(conf_file, origin_city, destination_city, departure)
-                            else:
-                                output_HTML = executeJavaScript(conf_file, origin_city, destination_city, departure)
-                            travels = travels + extractData(conf_file, output_HTML)
+                            output_HTML = createURL(conf_file, origin_city, destination_city, departure)
+                            travels = travels + extractData(conf_file, output_HTML, origin_city, destination_city)
                     else:
-                        if(conf_file["webpage"]["javascript"] == False):
-                            output_HTML = createURL(conf_file, origin_city, destination_city, datetime.today().date())
-                        else:
-                            output_HTML = executeJavaScript(conf_file, origin_city, destination_city, datetime.today().date())
-                        travels = travels + extractData(conf_file, output_HTML)
-    else:
-        phantom.get(url)
-        time.sleep(conf_file["webpage"]["sleep_time"])
-        output_HTML = BeautifulSoup(phantom.page_source, "html.parser")
-        travels = extractData(conf_file, output_HTML)
+                        output_HTML = createURL(conf_file, origin_city, destination_city, datetime.today().date())
+                        travels = travels + extractData(conf_file, output_HTML, origin_city, destination_city)
+    elif(page_type == 2): #Javascript type pages
+        for origin_city in cities:
+            for destination_city in cities:
+                if(origin_city.id != destination_city.id):
+                    if(conf_file["webpage"]["frecuency_format"] == ""):
+                        for departure in dates:
+                            output_HTML = executeJavaScript(conf_file, origin_city, destination_city, departure)
+                            travels = travels + extractData(conf_file, output_HTML, origin_city, destination_city)
+                    else:
+                        output_HTML = output_HTML = executeJavaScript(conf_file, origin_city, destination_city, datetime.today().date())
+                        travels = travels + extractData(conf_file, output_HTML, origin_city, destination_city)
+    elif(page_type == 3): #Simple type pages
+        origin_cities = []
+        destination_cities = []
+        HTML_blocks = []
+        extractBlocks(conf_file, origin_cities, destination_cities, HTML_blocks)
+        for block in HTML_blocks:
+            travels += extractData(conf_file, block, origin_cities[counter], destination_cities[counter])
+
+        print origin_cities
+        print "\n"
+        print destination_cities
+        print "\n"
+        print HTML_blocks
 
     """with transaction.atomic():
         Travel.objects.filter(webpage = webpage_name).delete()
@@ -123,8 +137,69 @@ def executeJavaScript(conf_file, origin_city, destination_city, departure):
     #ciudad de destino y fecha de partida
     return ""
 
+def extractBlocks(conf_file, origin_cities, destination_cities, HTML_blocks):
+    phantom = webdriver.PhantomJS()
+    phantom.get(conf_file["webpage"]["uri_start"])
+    time.sleep(conf_file["webpage"]["sleep_time"])
+    soup = BeautifulSoup(phantom.page_source, "html.parser")
 
-def extractData(conf_file, html_file):
+    HTML_blocks = [soup]
+    aux_origin_cities = [soup]
+    aux_destination_cities = [soup]
+
+    #We extract the travel blocks
+    for tag in conf_file["webpage"]["iterators"]["travel_block"]:
+        sub_blocks = []
+        for block in HTML_blocks:
+            if((tag["field_type"] == "") and ("position" not in tag)):
+                sub_blocks += block.find_all(tag["tag_type"])
+            elif((tag["field_type"] == "") and ("position" in tag)):
+                sub_blocks += block.find_all(tag["tag_type"])[tag["position"]]
+            elif(tag["field_type"] != "") and ("position" not in tag):
+                sub_blocks += soup.find_all(tag["tag_type"],{tag["field_type"]: tag["name"]})
+            elif(tag["field_type"] != "") and ("position" in tag):
+                sub_blocks += soup.find_all(tag["tag_type"],{tag["field_type"]: tag["name"]})[tag["position"]]
+        HTML_blocks = sub_blocks[:]
+
+    #We extract the origin city of each travel block
+    for tag in conf_file["webpage"]["iterators"]["origin_city"]["fields"]:
+        sub_blocks = []
+        for block in aux_origin_cities:
+            if((tag["field_type"] == "") and ("position" not in tag)):
+                sub_blocks += block.find_all(tag["tag_type"])
+            elif((tag["field_type"] == "") and ("position" in tag)):
+                sub_blocks += block.find_all(tag["tag_type"])[tag["position"]]
+            elif(tag["field_type"] != "") and ("position" not in tag):
+                sub_blocks += soup.find_all(tag["tag_type"],{tag["field_type"]: tag["name"]})
+            elif(tag["field_type"] != "") and ("position" in tag):
+                sub_blocks += soup.find_all(tag["tag_type"],{tag["field_type"]: tag["name"]})[tag["position"]]
+        aux_origin_cities = sub_blocks[:]
+
+    for aux_origin_city in aux_origin_cities:
+        if (aux_origin_city != "\n"):
+            origin_cities += [unidecode(aux_origin_city.getText())]
+
+    #We extract the destination city of each travel block
+    for tag in conf_file["webpage"]["iterators"]["destination_city"]["fields"]:
+        sub_blocks = []
+        for block in aux_destination_cities:
+            if((tag["field_type"] == "") and ("position" not in tag)):
+                sub_blocks += block.find_all(tag["tag_type"])
+            elif((tag["field_type"] == "") and ("position" in tag)):
+                sub_blocks += block.find_all(tag["tag_type"])[tag["position"]]
+            elif(tag["field_type"] != "") and ("position" not in tag):
+                sub_blocks += soup.find_all(tag["tag_type"],{tag["field_type"]: tag["name"]})
+            elif(tag["field_type"] != "") and ("position" in tag):
+                sub_blocks += soup.find_all(tag["tag_type"],{tag["field_type"]: tag["name"]})[tag["position"]]
+        aux_destination_cities = sub_blocks[:]
+
+    for aux_destination_city in aux_destination_cities:
+        if (aux_destination_city != "\n"):
+            destination_cities += [unidecode(aux_destination_city.getText())]
+
+    print explosion[900]
+
+def extractData(conf_file, html_file, origin_city, destination_city):
     #TODO: extraer los datos del HTML con el archivo de configuracion, transformarlos en instancias de
     #Travel y devolverlos
     return []
