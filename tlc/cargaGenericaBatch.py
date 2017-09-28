06f5fb2d0c169c134f84d7d7513b89e727a9b50c
Juan <benelli_91@hotmail.com>
2017-09-28 18:15:55 -0300

    implementacion parcial de extraccion de datos

--- tlc/cargaGenericaBatch.py
+++ tlc/cargaGenericaBatch.py
@@ -210,4 +210,81 @@ def extractBlocks(conf_file, origin_cities, destination_cities, HTML_blocks):
 def extractData(conf_file, html_file, origin_city, destination_city):
     #TODO: extraer los datos del HTML con el archivo de configuracion, transformarlos en instancias de
     #Travel y devolverlos
+
+    if(conf_file["webpage"]["simple_page"] == False):
+        #departure extraction_tags
+        departure_fields = conf_file["webpage"]["extraction_tags"]["departure"]["fields"]
+        departure_format = conf_file["webpage"]["extraction_tags"]["departure"]["format"]
+        departure_formula = conf_file["webpage"]["extraction_tags"]["departure"]["formula"]
+        #price extraction_tags
+        price_fields = conf_file["webpage"]["extraction_tags"]["price"]["fields"]
+        price_format = conf_file["webpage"]["extraction_tags"]["price"]["format"]
+        price_formula = conf_file["webpage"]["extraction_tags"]["price"]["formula"]
+        #duration extraction_tags
+        duration_fields = conf_file["webpage"]["extraction_tags"]["duration"]["fields"]
+        duration_format = conf_file["webpage"]["extraction_tags"]["duration"]["format"]
+        duration_formula = conf_file["webpage"]["extraction_tags"]["duration"]["formula"]
+        #travel_agency extraction_tags
+        travel_agency_fields = conf_file["webpage"]["extraction_tags"]["travel_agency"]["fields"]
+        travel_agency_format = conf_file["webpage"]["extraction_tags"]["travel_agency"]["format"]
+        travel_agency_formula = conf_file["webpage"]["extraction_tags"]["travel_agency"]["formula"]
+        #frequency extraction_tags
+        frequency_agency_fields = conf_file["webpage"]["extraction_tags"]["frequency"]["fields"]
+        frequency_agency_format = conf_file["webpage"]["extraction_tags"]["frequency"]["format"]
+        frequency_agency_formula = conf_file["webpage"]["extraction_tags"]["frequency"]["formula"]
+
+        departure_list = get_data_list(departure_fields,html_file)
+        price_list = get_data_list(price_fields,html_file)
+        duration_list = get_data_list(duration_fields,html_file)
+        travel_agency_list = get_data_list(travel_agency_fields,html_file)
+        frequency_agency_list = get_data_list(frequency_agency_fields,html_file)
+
+        for xx in range(len(departure_list)):
+            print departure_list[xx]
+            print price_list[xx].contents
+            #print duration_list[xx].contents
+            #print travel_agency_list[xx].contents
+            #print frequency_agency_list[xx]
+    else:
+        a = 1
     return []
+
+def get_data_list(fields_list,html_file):
+    #print html_file
+    result = None
+    aux_html = html_file
+    first = True
+    for item in fields_list: #for each item in the field list in the config_file
+        if not first: #the first time i have the defaul value in "html_file"
+            aux_html = result #In the following times I have to work with the previous result
+        first = False
+
+        find_tag = ''
+        find_field = ''
+        find_name  = ''
+        find_position = 999999999999 #invalid position value
+        #check if the item in the config_file has the tag
+        if item.has_key("tag_type"):
+            find_tag = item["tag_type"]
+        if item.has_key("field_type"):
+            find_field = item["field_type"]
+        if item.has_key("name"):
+            find_name = item["name"]
+        if item.has_key("position"):
+            find_position = item["position"]
+
+        if find_position != 999999999999: #if i need to find a particular child
+            text_result = ''
+            for child in aux_html:
+                soup = BeautifulSoup(TRIPLE_QUOTES + str(child) + TRIPLE_QUOTES,"html.parser")
+                aux_result = soup.find_all(find_tag,{find_field:find_name})
+                actual_position = 0
+                for litle_child in aux_result:
+                    if actual_position == find_position:
+                        text_result += str(litle_child)
+                    actual_position += 1
+            result = BeautifulSoup(TRIPLE_QUOTES + text_result + TRIPLE_QUOTES,"html.parser").find_all()
+        else:
+            soup = BeautifulSoup(TRIPLE_QUOTES + str(aux_html) + TRIPLE_QUOTES,"html.parser")
+            result =soup.find_all(find_tag,{find_field:find_name})
+    return result
