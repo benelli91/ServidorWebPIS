9900c118751bf29bd3c39eef3dc2ffceded82bca
GuillermoAlvez <GuillermoAlvez>
2017-10-07 13:02:52 -0300

    no grid

--- servidorwebpis/settings.py
+++ servidorwebpis/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'servidorwebpis.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pisunitario',
-        'USER': 'pis',
+        'NAME': 'pis_db',
+        'USER': 'jalvez',
         'PASSWORD': 'pis2017...',
-        'HOST': 'lowcosttrip.ddns.net',
+        'HOST': 'localhost',
         'PORT': '',
     }
 }
