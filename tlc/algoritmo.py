d3930edb90648345af874460ed09086398307c8d
Juan <benelli_91@hotmail.com>
2017-11-08 02:37:03 -0300

    mejoras de tiempos

--- tlc/algoritmo.py
+++ tlc/algoritmo.py
@@ -21,32 +21,31 @@ def load_exchanges():
 
 def load_travels(origin_city, start_date):
 
-    start_time = time.clock()
     processed_cities[origin_city] = {}
 
     first_travels_day_1 = Travel.objects.filter(
         origin_city = origin_city,
         departure__gte = start_date,
         departure__lte = start_date + timedelta(hours=12)
-    ).order_by('departure').distinct('departure','destination_city','price','duration','traveltype')
+    ).order_by('departure','price','duration').distinct('departure','destination_city','price','duration','traveltype')
 
     last_travels_day_1 = Travel.objects.filter(
         origin_city = origin_city,
         departure__gte = start_date + timedelta(hours=12),
         departure__lte = start_date + timedelta(hours=24)
-    ).order_by('departure').distinct('departure','destination_city','price','duration','traveltype')
+    ).order_by('departure','price','duration').distinct('departure','destination_city','price','duration','traveltype')
 
     first_travels_day_2 = Travel.objects.filter(
         origin_city = origin_city,
         departure__gte = start_date + timedelta(hours=24),
         departure__lte = start_date + timedelta(hours=36)
-    ).order_by('departure').distinct('departure','destination_city','price','duration','traveltype')
+    ).order_by('departure','price','duration').distinct('departure','destination_city','price','duration','traveltype')
 
     last_travels_day_2 = Travel.objects.filter(
         origin_city = origin_city,
         departure__gte = start_date + timedelta(hours=36),
         departure__lte = start_date + timedelta(hours=48)
-    ).order_by('departure').distinct('departure','destination_city','price','duration','traveltype')
+    ).order_by('departure','price','duration').distinct('departure','destination_city','price','duration','traveltype')
 
 
     first_travels_day_3 = Travel.objects.filter(
@@ -54,15 +53,14 @@ def load_travels(origin_city, start_date):
         Q(departure__gte = start_date + timedelta(hours=48)) &
         Q(departure__lte = start_date + timedelta(hours=60)) &
         Q(departure__lte = start_date + (timedelta(minutes=1) * F("duration")))
-    ).order_by('departure').distinct('departure','destination_city','price','duration','traveltype')
+    ).order_by('departure','price','duration').distinct('departure','destination_city','price','duration','traveltype')
 
     last_travels_day_3 = Travel.objects.filter(
         Q(origin_city = origin_city) &
         Q(departure__gte = start_date + timedelta(hours=48)) &
         Q(departure__lte = start_date + (timedelta(minutes=1) * F("duration")))
-    ).order_by('departure').distinct('departure','destination_city','price','duration','traveltype')
+    ).order_by('departure','price','duration').distinct('departure','destination_city','price','duration','traveltype')
 
-    count_time['sql_time'] += time.clock() - start_time
 
     # The current origin_city is added to the set of processed cities
     processed_cities[origin_city]['first_travels_d1'] = list(first_travels_day_1)
@@ -75,7 +73,6 @@ def load_travels(origin_city, start_date):
     processed_cities[origin_city]['last_travels_d3'] = list(last_travels_day_3)
 
 def get_travels_for_origin_city(origin_city, current_date, start_date):
-    start_time = time.clock()
 
     time_diff = current_date - start_date
 
@@ -114,7 +111,6 @@ def get_travels_for_origin_city(origin_city, current_date, start_date):
     else:
         from_origin_city_travels = processed_cities[origin_city]['last_travels_d3']
 
-    count_time['armar_lista_travels_time'] += time.clock() - start_time
     return from_origin_city_travels
 
 def sortVectores(travel_list, travels_price_list, quantity_travels):
@@ -130,17 +126,13 @@ def sortVectores(travel_list, travels_price_list, quantity_travels):
     while i < quantity_travels[0] :
         actual_price = float_price_list[i]
         actual_element = travel_list[i]
-        actual_duration = 0
-        for t in actual_element:
-            actual_duration += t.duration
+        actual_duration = actual_element[-1].departure - actual_element[0].departure + timedelta(minutes=actual_element[-1].duration)
         minimal_price = actual_price
         minimal_duration = actual_duration
         j = i
         while j <= quantity_travels[0] -1:
             if float_price_list[j] <= minimal_price:
-                duration_aux = 0
-                for t in travel_list[j]:
-                    duration_aux += t.duration
+                duration_aux = travel_list[j][-1].departure - travel_list[j][0].departure + timedelta(minutes=travel_list[j][-1].duration)
                 if float_price_list[j] == minimal_price:
                     if duration_aux < minimal_duration:
                         minimal_price = float_price_list[j]
@@ -166,20 +158,13 @@ def sortVectores(travel_list, travels_price_list, quantity_travels):
 
     return (travel_list,travels_price_list)
 
-def have_i_passed(t,list_trips_traveled):
-    aux_time = time.clock()
-    passed = t.destination_city.id in [travel.origin_city.id for travel in list_trips_traveled]
-    count_time['check_ciclos_time'] += time.clock() -aux_time
-    return passed
-
 def verifyBestOption(list_trips_traveled,cost,travel_list,travels_price_list):
 
     exists_better_option = False
     exist_same_condition = False
     index_same_condition = 0
-    # duration = 0
-    # for t in list_trips_traveled:
-    #     duration += t.duration
+    # duration = list_trips_traveled[-1].departure - list_trips_traveled[0].departure + timedelta(minutes=list_trips_traveled[-1].duration)
+    #
     # first_travel = list_trips_traveled[0]
     # origin_city = first_travel.origin_city
     # departure = first_travel.departure
@@ -188,9 +173,7 @@ def verifyBestOption(list_trips_traveled,cost,travel_list,travels_price_list):
     # for travels in travel_list:
     #     travel_ini = travels[0]
     #     if origin_city.id == travel_ini.origin_city.id and str(departure) == str(travel_ini.departure) and str(cost) == str(travels_price_list[cont]):
-    #         duration_aux = 0
-    #         for t in travels:
-    #             duration_aux += t.duration
+    #         duration_aux = travels[-1].departure - travels[0].departure + timedelta(minutes=travels[-1].duration)
     #
     #         if duration < duration_aux:
     #             exists_better_option = False
@@ -208,17 +191,11 @@ def verifyBestOption(list_trips_traveled,cost,travel_list,travels_price_list):
 # Recursion de Backtracking
 #######################################
 def recursion(origin_city, destination_city, cost, start_date, current_date, maximum_date, travel_list, travels_price_list, list_trips_traveled, quantity_travels, max_scales, max_cost, cotizaciones):
-    count_time['recursion_time'] += time.clock() - count_time['aux_recursion_time']
 
     max_scales += 1
-    found_trip = False
-    found_trip_in_recurrence = False
-
     if max_scales <= 4:
-
         # [ Identification of the destination city is unique ]
         key_destination = str(destination_city)
-
         # Check if the city of origin was processed in another iteration:
         if not processed_cities.has_key(origin_city):
             # Otherwise, the query is made in the database:
@@ -228,14 +205,6 @@ def recursion(origin_city, destination_city, cost, start_date, current_date, max
 
         # For all trips that exist starting from the current city:
         for t in from_origin_city_travels:
-
-            # The price is modified for travels with currencies other than USD
-            if t.currency != 'USD':
-                divider = cotizaciones[t.currency]
-                t.price = round(t.price / divider,2)
-                t.currency = 'USD'
-
-
             # If is the first iteration
             # => Verify that the trip starts on the start date
             if current_date == start_date:
@@ -245,32 +214,19 @@ def recursion(origin_city, destination_city, cost, start_date, current_date, max
 
             # If for the date the condition is true:
             if date_condition:
-
                 key_travel_destination = str(t.destination_city.id)
+                if not key_travel_destination in cities_visited:
+                    # The price is modified for travels with currencies other than USD
+                    if t.currency != 'USD':
+                        divider = cotizaciones[t.currency]
+                        t.price = round(t.price / divider,2)
+                        t.currency = 'USD'
 
-                # Conversion to hours and minutes of the duration of travel
-                minutes  = t.duration % 60
-                hours = (t.duration - minutes) / 60
-
-                # The current_date is updated
-                aux_current_date = t.departure + timedelta(hours=hours, minutes = minutes)
-
-                #lo separo para poder tomar el tiempo de resolucion
-                aux_time = time.clock()
-                count_time['check_ciclos_time'] += time.clock() -aux_time
-                
-                #if not have_i_passed(t,list_trips_traveled):
-                if not t.destination_city.id in cities_visited:
-                    # If have not visited this city, look for a way to the destination
                     cost += t.price
-                    #list_trips_traveled[len(list_trips_traveled):] = [t]
-                    list_trips_traveled.append(t)
-                    
-                    # cities_visited[len(cities_visited):] = [t.destination_city.id]
-                    if t.destination_city.id not in cities_visited:
-                        cities_visited.append(t.destination_city.id)
-
                     if cost <= max_cost[0] or quantity_travels[0] < 10:
+                        # If have not visited this city, look for a way to the destination
+                        list_trips_traveled.append(t)
+
                         # If the destination of the trip is the city you are looking for,
                         # Add the way traveled to the travel list
                         if key_travel_destination == key_destination:
@@ -280,27 +236,17 @@ def recursion(origin_city, destination_city, cost, start_date, current_date, max
                             if not exists_better_option:
                                 # Made a copy from list_trips_traveled,
                                 # to avoid sharing memory between lists.
-                                
-                                # aux_list = []
-                                # x = 0
-                                # for x in xrange(len(list_trips_traveled)):
-                                #     aux_list.append(list_trips_traveled[x])
-                                
                                 aux_list = list_trips_traveled[:]
-                                
+
                                 if exist_same_condition:
                                     # travel_list.remove(travel_list[index_same_condition])
                                     # travels_price_list.remove(travels_price_list[index_same_condition])
                                     del travel_list[index_same_condition]
                                     del travels_price_list[index_same_condition]
-                                    # travel_list[len(travel_list):]= [aux_list]
-                                    travel_list += aux_list
-                                    #travels_price_list[len(travel_list):]= [str(cost)]
+                                    travel_list += [aux_list]
                                     travels_price_list.append(str(cost))
                                 else:
-                                    # travel_list[len(travel_list):]= [aux_list]
-                                    travel_list += aux_list
-                                    #travels_price_list[len(travel_list):]= [str(cost)]
+                                    travel_list += [aux_list]
                                     travels_price_list.append(str(cost))
 
                                     if quantity_travels[0] >= 10:
@@ -308,56 +254,31 @@ def recursion(origin_city, destination_city, cost, start_date, current_date, max
                                         index = float_price_list.index(max(float_price_list))
                                         travels_price_list.remove(travels_price_list[index])
                                         travel_list.remove(travel_list[index])
-                                        max_cost[0] = max([float_price_list])
+                                        max_cost[0] = max(float_price_list)
                                     else:
                                         quantity_travels[0] += 1
 
-                                    if cost > max_cost[0]:
-                                        max_cost[0] = cost
+                                        if cost > max_cost[0]:
+                                            max_cost[0] = cost
 
-                                found_trip = True
+
                         # If the destination of the trip isn't the city you are looking for,
                         else:
+                            cities_visited.append(str(t.destination_city.id))
+                            # Conversion to hours and minutes of the duration of travel
+                            minutes  = t.duration % 60
+                            hours = (t.duration - minutes) / 60
+                            # The current_date is updated
+                            aux_current_date = t.departure + timedelta(hours=hours, minutes = minutes)
+
                             # The destination of the processed trip becomes the origin of the next iteration
                             origin_city = key_travel_destination
-                            count_time['aux_recursion_time'] = time.clock()
-                            aux_encontro_recursion = recursion(origin_city, destination_city, cost,start_date,aux_current_date,maximum_date,travel_list,travels_price_list,list_trips_traveled,quantity_travels,max_scales,max_cost,cotizaciones)
-                            count_time['recursion_time'] += time.clock() - count_time['aux_recursion_time']
-                            found_trip_in_recurrence = found_trip_in_recurrence or aux_encontro_recursion
+                            recursion(origin_city, destination_city, cost,start_date,aux_current_date,maximum_date,travel_list,travels_price_list,list_trips_traveled,quantity_travels,max_scales,max_cost,cotizaciones)
 
-                            if not aux_encontro_recursion and 1 == 2:
-                                # list_to_delete = []
-                                # if processed_cities.has_key(origin_city):
-                                #     for aux_travel in processed_cities.get(origin_city):
-                                #         if aux_travel.departure >= aux_current_date:
-                                #             list_to_delete[len(list_to_delete):] = [aux_travel]
-                                #
-                                #     for travel_to_remove in list_to_delete:
-                                #         processed_cities.get(origin_city).remove(travel_to_remove)
-                                if processed_cities[origin_city]['first_travels_d1'].count(t):
-                                    processed_cities[origin_city]['first_travels_d1'].remove(t)
+                            cities_visited.pop()
 
-                                if processed_cities[origin_city]['last_travels_d1'].count(t):
-                                    processed_cities[origin_city]['last_travels_d1'].remove(t)
-
-                                if processed_cities[origin_city]['first_travels_d2'].count(t):
-                                    processed_cities[origin_city]['first_travels_d2'].remove(t)
-
-                                if processed_cities[origin_city]['last_travels_d2'].count(t):
-                                    processed_cities[origin_city]['last_travels_d2'].remove(t)
-
-                                if processed_cities[origin_city]['first_travels_d3'].count(t):
-                                    processed_cities[origin_city]['first_travels_d3'].remove(t)
-
-                                if processed_cities[origin_city]['last_travels_d3'].count(t):
-                                    processed_cities[origin_city]['last_travels_d3'].remove(t)
-
-
-                    list_trips_traveled.pop()
-                    cities_visited.pop()
+                        list_trips_traveled.pop()
                     cost -= t.price
-    count_time['aux_recursion_time'] = time.clock()
-    return found_trip or found_trip_in_recurrence
 
 ################################################
 # Do_Search [Backtracking First call]
@@ -367,7 +288,7 @@ def do_search(origin_city, destination_city, date, timezone):
     # The requested date is returned to TimeZone 0 (Date - timezone offset)
 
     global count_time
-    count_time = {'sql_time':0,'total_time':time.clock(),'recursion_time':0,'check_ciclos_time':0,'armar_lista_travels_time' : 0 }
+    count_time = {'total_time':time.clock()}
 
 
     initial_date = datetime.strptime(date+' 12:00AM', '%m/%d/%Y %I:%M%p') + timedelta(minutes=-timezone)
@@ -384,13 +305,12 @@ def do_search(origin_city, destination_city, date, timezone):
     processed_cities = {}
 
     global cities_visited
-    cities_visited = [origin_city]
+    cities_visited = [str(origin_city)]
 
     # The minimum and maximum duration of a trip is determined
     current_date = initial_date
     maximum_date= initial_date + timedelta(days=3)
 
-    count_time['aux_recursion_time'] = time.clock()
     # Recursive calls of Backtracking
     recursion(origin_city,
         destination_city,
@@ -406,17 +326,12 @@ def do_search(origin_city, destination_city, date, timezone):
         max_cost,
         cotizaciones
     )
-    count_time['recursion_time'] += time.clock() - count_time['aux_recursion_time']
     # The results of the recursion are sorted
     travel_list,travels_price_list = sortVectores(travel_list, travels_price_list, quantity_travels)
 
 
     count_time['total_time'] = time.clock() - count_time['total_time']
 
-    print 'SQL' , count_time['sql_time']
-    print 'llamado recursivo', count_time['recursion_time']
-    print 'armar_lista_travels_time', count_time['armar_lista_travels_time']
-    print 'check_ciclos_time', count_time['check_ciclos_time']
     print 'total', count_time['total_time']
 
 
