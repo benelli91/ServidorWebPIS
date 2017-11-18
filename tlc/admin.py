2300cb8953e59d8d15e5b126aa2d2a9d56bb1c85
Joaquin_Scocozza <joaquinscocozza94@hotmail.com>
2017-11-18 11:14:02 -0300

    Se agrega TravelAgency y Currency al admin de Django :)

--- tlc/admin.py
+++ tlc/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
-from .models import City, Country, Travel, Traveltype
+from .models import City, Country, Travel, Traveltype, Travelagency, Currency
 # Register your models here.
 
 @admin.register(City)
@@ -25,3 +25,13 @@ class TravelAdmin(admin.ModelAdmin):
 class TravelTypeAdmin(admin.ModelAdmin):
     search_fields = ( 'travelname', )
     list_display = ('traveltype', 'travelname')
+    
+@admin.register(Travelagency)
+class TravelagencyAdmin(admin.ModelAdmin):
+    search_fields = ( 'name', )
+    list_display = ('id', 'name', 'reference', 'traveltype' )
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    search_fields = ( 'name', )
+    list_display = ('cod', 'name', 'base', 'local', 'divisor', 'tableposition')
